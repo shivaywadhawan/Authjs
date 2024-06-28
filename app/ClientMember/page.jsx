bc7bf9efee0dc934a00311b4977b72c00e30c66e
shivaywadhawan <shivaywadhawan@gmail.com>
2024-06-28 11:48:26 -0400

    Added server side authentication

--- app/ClientMember/page.jsx
+++ app/ClientMember/page.jsx
@@ -1,8 +1,21 @@
-"use Client";
-function Member() {
+"use client";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+
+async function Member() {
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/api/auth/signin?callbackUrl=/ClientMember");
+    },
+  });
   return (
-    <div><h1>Member Client Session</h1></div>
-  )
+    <div>
+      <h1>Member Client Session</h1>
+      <p>{session?.user?.email}</p>
+      <p>{session?.user?.role}</p>
+    </div>
+  );
 }
 
-export default Member
+export default Member;
--- app/Member/page.jsx
+++ app/Member/page.jsx
@@ -1,7 +1,19 @@
-function Member() {
+import { getServerSession } from "next-auth";
+import { options } from "../api/auth/[...nextauth]/options";
+import { redirect } from "next/navigation";
+
+async function Member() {
+  const session = await getServerSession(options);
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/Member");
+  }
   return (
-    <div><h1>Member Server Session</h1></div>
-  )
+    <div>
+      <h1>Member Server Session</h1>
+      <p>{session?.user?.email}</p>
+      <p>{session?.user?.role}</p>
+    </div>
+  );
 }
 
-export default Member
+export default Member;
